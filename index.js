f6b1b6eae23c33eaf74f79461f5aa4a2c1290cfb
Julie Erlemeier <jmerlemeier@gmail.com>
2019-10-17 09:38:50 -0700

    Reader completed with error handling.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 //entry file
 
 //add the ability to actually add files look at fs write file methods
@@ -6,3 +8,18 @@
 //findout what is in the file, how can I change it and rewrite the file
 // redo as promise and await
 // add tests
+
+const file = process.argv[2]; //looks at command line input. 2nd index because file name.
+
+const useCallback = require('./lib/file-edit-callback');
+// const usePromise = require('./lib/file-edit-promise');
+// const useAwait = require('./lib/file-edit-await');
+
+//Handles the reader
+useCallback.reader(file, (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
